a603d8665097c1390d0a81aac68fca39f47691db
007AxelBerkati <bucel1916@gmail.com>
2022-02-02 18:25:33 +0700

    integration - Login session dan update profile

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -1,16 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { ILNullPhoto } from '../../assets';
 import { Gap, Header, List, Profile } from '../../components';
-import { colors } from '../../utils';
-import { NavigationContainer } from '@react-navigation/native';
+import { colors, getData } from '../../utils';
 
 export default function UserProfile({ navigation }) {
+  const [profile, setProfile] = useState({
+    fullName: '',
+    profession: '',
+    photo: ILNullPhoto,
+  });
+  useEffect(() => {
+    getData('user').then((res) => {
+      const data = res;
+      data.photo = { uri: res.photo };
+      setProfile(data);
+    });
+  }, []);
+
   return (
     <View style={styles.page}>
       <Header title={'Profile'} onPress={() => navigation.goBack()} />
       <Gap height={10} />
-      <Profile name={'Lady Gaga'} desc={'Product Designer'} />
+      {profile.fullName.length > 0 && (
+        <Profile name={profile.fullName} desc={profile.profession} photo={profile.photo} />
+      )}
       <Gap height={14} />
       <List
         name={'Edit Profile'}
